396adfb9a4971d40b2089a798c47b64ae9bc4fc9
Sandeep Dongapure <sandeeprd07@gmail.com>
2016-07-15 19:29:57 +0200

    Created project for Crtification

---
+++ src/main/java/chapter1/Certification.java
@@ -0,0 +1,33 @@
+package chapter1;
+
+/**
+ * Created by Sandeep on 15-07-2016.
+ */
+public class Certification {
+
+    private String name;
+    private String version;
+
+    /*
+    sd,nfkjsdnf
+    zs,jdnfjsndf
+    szdkjfklsjdfkl
+    ,jhzcjh,dnfil,mn s,dnflkjdfk
+     */
+    public String getName() {
+        return name;
+    }
+
+    /*//cxvmb*/
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
